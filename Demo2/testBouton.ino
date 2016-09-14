5c52b429f2ed1514a444002c2fde76e1b91c0dd6
Arhimeas <arhimeas@gmail.com>
2016-09-14 16:00:02 +0200

    Add files via upload

---
+++ Demo2/testBouton.ino
@@ -0,0 +1,27 @@
+int etatBouton = 0;
+int pinBouton = 2;
+int previousEtat = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(13,OUTPUT);
+}
+
+void loop() {
+  etatBouton = digitalRead(pinBouton);
+  if (etatBouton != previousEtat)
+  {
+    if (etatBouton)
+    {
+      Serial.println("Il est vivant ! VIVAAAAAAAANT !!!!!!");
+      digitalWrite(13,HIGH);
+    }
+    else
+    {
+      Serial.println("Il est decedu :'((((((( ");
+      digitalWrite(13,LOW);
+    }
+  }
+  previousEtat = etatBouton;
+  delay(100);
+}
